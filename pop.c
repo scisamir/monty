86bbf2ac02085f617b7d99656bacb351f8ebfa35
Samir Idris <samzidris11@gmail.com>
2023-10-22 11:24:28 +0100

    pop opcode

--- pop.c
+++ pop.c
@@ -12,8 +12,6 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = NULL;
 
-	pall(stack, line_number);
-
 	if (*stack == NULL)
 	{
 		dprintf(2, "L%d: can't pop an empty stack\n", line_number);
